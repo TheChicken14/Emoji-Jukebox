15ea5f6197a32cd1fb0c34229310004a3ec2bfb2
Wisse <wissehes@gmail.com>
2021-05-01 23:48:08 +0200

    Create controls

---
+++ src/components/Controls.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <span class="control" @click="prev">⏪</span>
+    <span class="space" />
+
+    <span class="control" @click="shuffle">🔀</span>
+    <span class="space" />
+
+    <span class="control" @click="next">⏩</span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    prev() {
+      this.$store.commit("previous");
+    },
+    shuffle() {
+      this.$store.commit("shuffle");
+    },
+    next() {
+      this.$store.commit("next");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.control {
+  font-size: 4rem;
+  cursor: pointer;
+}
+
+.space {
+  margin: 0.5rem;
+}
+</style>
