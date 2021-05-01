f16503708213b6e631937f0d6a10f92bcb64684d
Wisse <wissehes@gmail.com>
2021-05-02 00:52:02 +0200

    Created about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
+  <div class="container">
     <h1>hi</h1>
-    <p>this is just a simple thingy</p>
+    <p>
+      Basically this page presents you with a few emojis and you can guess which
+      song it means, then you click on the emojis and it plays the song on
+      YouTube.
+    </p>
+    <p>You can skip through the songs with the buttons.</p>
+
+    <p>
+      Made by
+      <a href="https://twitter.com/skeppy_chicken" target="_blank">Wisse</a>
+    </p>
   </div>
 </template>
+
+<style scoped>
+.container {
+  padding: 1rem;
+  font-size: 1rem;
+}
+</style>
