c44bc58e06c3c749f7bd9742187fe1b6f7ea93a0
Wisse <wissehes@gmail.com>
2021-05-01 22:09:36 +0200

    Added songs

---
+++ src/songs.js
@@ -0,0 +1,102 @@
+export default [
+  {
+    url: "https://youtu.be/wagn8Wrmzuc?t=33",
+    name: "Lady Gaga - Judas",
+    emoji: "🛐🧔‍♂️",
+  },
+  {
+    url: "https://youtu.be/qrO4YZeyl0I",
+    name: "Lady Gaga - Bad Romance",
+    emoji: "👩‍❤️‍👩 ❌",
+  },
+  {
+    url: "https://youtu.be/bESGLojNYSo",
+    name: "Lady Gaga - Poker Face",
+    emoji: "♣️😩",
+  },
+  {
+    url: "https://youtu.be/PNu_-deVemE?t=203",
+    name: "Lady Gaga - G.U.Y.",
+    emoji: "👧⬇️☝️",
+  },
+  {
+    url: "https://youtu.be/niqrrmev4mA?t=62",
+    name: "Lady Gaga - Alejandro",
+    emoji: "👩❌🧍‍♂️",
+  },
+  {
+    url: "https://youtu.be/EVBsypHzF3U?t=174",
+    name: "Lady Gaga ft. Beyoncé - Telephone",
+    emoji: "👩📞",
+  },
+  {
+    url: "https://youtu.be/AoAm4om0wTs",
+    name: "Lady Gaga & Ariana Grande - Rain On Me",
+    emoji: "🌧🧍🌧",
+  },
+  {
+    url: "https://youtu.be/58hoktsqk_Q",
+    name: "Lady Gaga - 911",
+    emoji: "9️⃣1️⃣1️⃣",
+  },
+  {
+    url: "https://youtu.be/5L6xyaeiV58",
+    name: "Lady Gaga - Stupid Love",
+    emoji: "❤️👹",
+  },
+  {
+    url: "https://youtu.be/eRAN7AzEKak?t=28",
+    name: "Senhit - Adrenalina",
+    emoji: "🎢🇸🇲",
+  },
+  {
+    url: "https://youtu.be/FSTMz-_kbVQ?t=5",
+    name: "Hurricane - Loco Loco",
+    emoji: "💃🎊🇷🇸",
+  },
+  {
+    url: "https://youtu.be/PQKiHr5qEfA?t=27",
+    name: "Destiny - Je Me Casse",
+    emoji: "👧💅🇲🇹",
+  },
+  {
+    url: "https://youtu.be/vuq-VAiW9kw?t=41",
+    name: "Lizzo - Good As Hell",
+    emoji: "👩🏽💅💅",
+  },
+  {
+    url: "https://youtu.be/XaCrQL_8eMY",
+    name: "Lizzo - Juice",
+    emoji: "👩🏽🥤",
+  },
+  {
+    url: "https://youtu.be/IHS_2GpNwmA?t=5",
+    name: "Albina - Tick Tock",
+    emoji: "🕰🕰🇭🇷",
+  },
+  {
+    url: "https://youtu.be/l01wa2ChX64?t=5",
+    name: "Manizha - Russian Woman (live)",
+    emoji: "👩🇷🇺",
+  },
+  {
+    url: "https://youtu.be/5P1ueI9j6gk?t=5",
+    name: "Tusse - Voices (live)",
+    emoji: "🗣🇸🇪",
+  },
+  {
+    url: "https://youtu.be/Dkk9gvTmCXY",
+    name: "Taylor Swift - You Need To Calm Down",
+    emoji: "🧍😌",
+  },
+  {
+    url: "https://youtu.be/7GUsLvUvSu0?t=10",
+    name: "KEiiNO - MONUMENT (live)",
+    emoji: "🗿🇳🇴",
+  },
+  {
+    url: "https://youtu.be/R3D-r4ogr7s",
+    name: "Duncan Laurence - Arcade (live)",
+    emoji: "🕹🏆🇳🇱",
+  },
+];
