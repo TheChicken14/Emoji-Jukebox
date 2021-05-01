fac12127890df961d9cf7688b114ee7cbb752842
Wisse <wissehes@gmail.com>
2021-05-01 23:48:16 +0200

    Show the actual emoji

---
+++ src/components/Emoji.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 class="emoji" :title="name">
+    <a :href="url" class="url" target="_blank">{{ emoji }}</a>
+  </h1>
+</template>
+
+<script>
+export default {
+  computed: {
+    emoji() {
+      return this.$store.state.currentSong.emoji;
+    },
+    url() {
+      return this.$store.state.currentSong.url;
+    },
+    name() {
+      return this.$store.state.currentSong.name;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.url {
+  text-decoration: none;
+}
+.emoji {
+  font-size: 6.5rem;
+}
+</style>
