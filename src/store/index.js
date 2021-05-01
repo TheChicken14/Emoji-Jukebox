be057651094efe9caaedbb3fa79c45dca9447829
Wisse <wissehes@gmail.com>
2021-05-01 22:14:34 +0200

    Create basic store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+import songs from "../songs";
 
 export default new Vuex.Store({
   state: {
+    currentSong: {
+      url: "",
+      name: "",
+      emoji: "",
+    },
   },
   mutations: {
+    previous(state) {
+      const index = songs.findIndex(state.currentSong);
+
+      if (songs[index - 1]) {
+        state.currentSong = songs[index - 1];
+      } else {
+        state.currentSong = songs[songs.length - 1];
+      }
+    },
+    shuffle(state) {
+      state.currentSong = songs[Math.floor(Math.random() * songs.length)];
+    },
+    next(state) {
+      const index = songs.findIndex(state.currentSong);
+
+      if (songs[index + 1]) {
+        state.currentSong = songs[index + 1];
+      } else {
+        state.currentSong = songs[0];
+      }
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
