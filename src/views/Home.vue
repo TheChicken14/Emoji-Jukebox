1fafcce124cbc9eabdc6e56b3940a363019d389c
Wisse <wissehes@gmail.com>
2021-05-08 14:04:40 +0200

    Add the youtube player to the views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <Emoji />
     <Controls />
+    <YouTubePlayer />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import Emoji from "../components/Emoji.vue";
 import Controls from "../components/Controls.vue";
+import YouTubePlayer from "../components/YouTubePlayer.vue";
 
 export default {
   name: "Home",
   components: {
     Emoji,
     Controls,
+    YouTubePlayer,
   },
 };
 </script>
